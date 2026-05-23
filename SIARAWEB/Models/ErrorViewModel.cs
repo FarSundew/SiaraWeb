289058842cb43df1d60d2f81346275678c8cee2d
Luis Fernando España Periañez <22470073@campeche.tecnm.mx>
2026-05-22 20:47:47 -0600

    Commit inicial: Proyecto ASP.NET Core MVC

---
+++ SIARAWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIARAWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
